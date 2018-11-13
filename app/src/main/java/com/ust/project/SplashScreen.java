8c869838042cabfd3cbf519495986df4a50a2880
AloysiusGChua <2015081433@ust-ics.mygbiz.com>
2018-11-14 00:22:55 +0800

    Update Splash Screen

--- app/src/main/java/com/ust/project/SplashScreen.java
+++ app/src/main/java/com/ust/project/SplashScreen.java
@@ -1,6 +1,7 @@
 package com.ust.project;
 
 import android.content.Intent;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -9,12 +10,15 @@ import android.os.Handler;
 
 public class SplashScreen extends AppCompatActivity {
 
-    private final int SPLASH_DISPLAY_LENGTH = 1000;
+    private final int SPLASH_DISPLAY_LENGTH = 2000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+        ActionBar actionBarMain = getSupportActionBar();
+        actionBarMain.hide();
 
         new Handler().postDelayed(new Runnable(){
             @Override
