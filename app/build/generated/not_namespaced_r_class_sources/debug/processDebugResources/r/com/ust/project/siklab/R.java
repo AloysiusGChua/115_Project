a24ced40c52af950666dd0087ac852b463384c7a
AloysiusGChua <2015081433@ust-ics.mygbiz.com>
2018-12-05 16:28:04 +0800

    Updated Backend-Reloj and Fixed UI-Chua

--- app/build/generated/not_namespaced_r_class_sources/debug/processDebugResources/r/com/ust/project/siklab/R.java
+++ app/build/generated/not_namespaced_r_class_sources/debug/processDebugResources/r/com/ust/project/siklab/R.java
@@ -3190,6 +3190,7 @@ public final class R {
   public static final class mipmap {
     public static final int ic_launcher=0x7f0b0000;
     public static final int ic_launcher_round=0x7f0b0001;
+    public static final int siklab=0x7f0b0002;
   }
   public static final class string {
     public static final int abc_action_bar_home_description=0x7f0c0000;
